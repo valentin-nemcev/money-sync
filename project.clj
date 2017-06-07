29f6f843de39d7dcee569d7b0e3000033703e871
Julia Nemtseva <juliaiskra@gmail.com>
2017-06-07 11:18:47 +0300

    Import libraries for joda time and joda money

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject money-sync "0.1.0-SNAPSHOT"
   :url "https://github.com/valentin-nemcev/money-sync"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/data.csv "0.1.4"]]
+                 [org.clojure/data.csv "0.1.4"]
+                 [clj-time "0.13.0"]
+                 [clojurewerkz/money "1.10.0"]]
   :main ^:skip-aot money-sync.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
